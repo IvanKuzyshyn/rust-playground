be5f86fab60d7030d2e5778456a27da14722b955
Ivan Kuzyshyn <ivan.ku@miro.com>
2023-10-08 18:04:56 +0200

    Implement centralized runner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod formatter;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: cargo run -- <module>");
+
+        return;
+    }
+
+    let module_name = &args[1];
+
+    match module_name.as_str() {
+        "formatter" => formatter::run(),
+        _ => eprintln!(
+            "Unknown module {}. Make sure it matches module in src folder.",
+            module_name
+        ),
+    }
 }
